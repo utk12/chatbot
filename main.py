f67590f5db41aa605d0053daf14e227ce4979689
Utkagr <agrawalutkarsh5@gmail.com>
2016-06-29 11:15:43 +0530

    few more changes

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from getFilters import *
 from applyFilters import *
 
 if __name__ == '__main__':
-	message = 'I want a in which should have cricket and tennis'
+	message = 'I want an apartment in which we should have football and vrv'
 	message =  main(message)
 	entities = get_entities_json_wit(message) #this is json object of entities from wit.
-	msg_geatures = get_entities_from_msg_Wit(message)
+	msg_features = get_entities_from_msg_Wit(message)
 
-	# dict_features = interpret_wit_output(entities)
-	# print dict_features
+	dict_features = interpret_wit_output(entities)
+	print dict_features
 	# print json.dumps(entities,indent = 4)
 	# print dict_features
 	# user_ft =  getFeatures(dict_features)
 	# updateUser('ddgpsaqf',user_ft)
-	# filters = wit_extract_filters(dict_features)
+	filters = wit_extract_filters(dict_features)
 	# print filters
-	# print getProjects(filters)
+	print getProjects(filters)
